32e0092742cc8737aa76485b6e27716df5827d1b
KaFai C <kafaicoder@gmail.com>
2016-01-03 21:07:58 +0800

    add postCard style

--- routes/views/index.js
+++ routes/views/index.js
@@ -18,6 +18,22 @@ exports = module.exports = function(req, res) {
 
 	locals.user = req.user;
 
+	locals.data = {
+		posts: [],
+	};
+
+	view.on('init', function(next) {
+
+		var q = keystone.list('Post').model.find().where('state', 'published').sort('-publishedDate').populate('author categories');
+
+		q.exec(function(err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+
+	});
+
+
 	view.render('site/index');
 
 }
