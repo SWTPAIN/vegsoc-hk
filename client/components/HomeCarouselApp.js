7a1d5188b6c750d41b8f7ffe73e6713e9f074d4c
KaFai C <kafaicoder@gmail.com>
2016-01-09 15:34:20 +0800

    upgrade react-responsive-carousel to v3

--- client/components/HomeCarouselApp.js
+++ client/components/HomeCarouselApp.js
@@ -4,10 +4,11 @@ import { Carousel } from 'react-responsive-carousel';
 export default class HomeCarouselApp extends Component {
   render() {
     return (
-      <Carousel type="slider" showControls={true} showStatus={true}>
-        <img src="http://leandrowd.github.io/react-responsive-carousel/assets/1.jpeg" />
-        <img src="http://leandrowd.github.io/react-responsive-carousel/assets/5.jpeg" />
+      <Carousel type="slider" showControls showStatus>
+        <img src="//c2.staticflickr.com/4/3665/12944904715_81fc31a117_h.jpg" />
+        <img src="//c2.staticflickr.com/4/3910/14370568981_2d49ecf1b7_n.jpg" />
+        <img src="//c1.staticflickr.com/1/56/130747772_537669005a_n.jpg" />
       </Carousel>
-    )
+    );
   }
 }
