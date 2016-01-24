b79aa2b8909885bb2425b952c6afbe3aa0482b13
SWTPAIN <kafaicoder@gmail.com>
2016-01-24 18:56:57 +0800

    working on animal Right page UI. finish header

--- routes/views/animalRights.js
+++ routes/views/animalRights.js
@@ -1,13 +1,11 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-
-	var view = new keystone.View(req, res),
-		locals = res.locals;
+	const view = new keystone.View(req, res);
+	const locals = res.locals;
 
 	locals.section = 'animal-rights';
 	locals.page.title = 'Animal Rights';
 
 	view.render('site/animalRights');
-
-}
+};
